d80f8bd388c95d783f75743a810d29eb302fe434
Mohimkhan <mohimkhan652@gmail.com>
2024-05-08 20:33:28 +0600

    Implement user login

---
+++ actions/user.js
@@ -0,0 +1,15 @@
+"use server";
+
+import { findUserByCredentials } from "@/services/db";
+
+export const performLogin = async (formData) => {
+  try {
+    const credential = {};
+    credential.email = formData.get("email");
+    credential.password = formData.get("password");
+    const found = await findUserByCredentials(credential);
+    return found;
+  } catch (error) {
+    throw error;
+  }
+};
--- services/db.js
+++ services/db.js
@@ -1,6 +1,8 @@
+import { userModel } from "@/models/user.models";
+import { replaceMongoIdInObject } from "@/utils/data-util";
 import mongoose from "mongoose";
 
-export async function dbConnect() {
+export const dbConnect = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       dbName: "eventry",
@@ -10,4 +12,12 @@ export async function dbConnect() {
   } catch (err) {
     console.log(err);
   }
-}
+};
+
+export const findUserByCredentials = async (credentials) => {
+  const user = await userModel.findOne(credentials).lean();
+  if (user) {
+    return replaceMongoIdInObject(user);
+  }
+  return null;
+};
