9219ce245c7f3f42d8b29d094edfbcf664a4e8c1
Mohimkhan <mohimkhan652@gmail.com>
2024-05-10 15:46:53 +0600

    Use auth info to display user name

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const { auth } = useAuth();
+  const [clientName, setClientName] = useState("");
+
+  useEffect(() => {
+    if (auth) {
+      setClientName(auth.name);
+    }
+  }, [auth]);
+
   return (
     <nav>
       <div className="container flex justify-between items-center py-4">
@@ -21,6 +34,7 @@ const Navbar = () => {
         </div>
 
         <ul className="flex gap-4 text-[#9C9C9C]">
+          {clientName && <li>{clientName}</li>}
           <li>About</li>
           <li>Contact Us</li>
         </ul>
