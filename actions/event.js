034aa95a09056de5241e9bf767c3fc23decf6454
Mohimkhan <mohimkhan652@gmail.com>
2024-05-11 19:26:15 +0600

    UpdateInterest action to update interested user

--- actions/event.js
+++ actions/event.js
@@ -1,20 +1,31 @@
 "use server";
 
 import { eventModel } from "@/models/event.models";
+import { updateInterest } from "@/services/db";
 
-import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/utils/data-util";
+import { revalidatePath } from "next/cache";
 
 async function getAllEvents() {
-    const allEvents = await eventModel.find().lean();
-    return replaceMongoIdInArray(allEvents);
+  const allEvents = await eventModel.find().lean();
+  return replaceMongoIdInArray(allEvents);
 }
 
 async function getEventById(eventId) {
-    const event = await eventModel.findById(eventId).lean();
-    return replaceMongoIdInObject(event);
+  const event = await eventModel.findById(eventId).lean();
+  return replaceMongoIdInObject(event);
 }
 
-export {
-    getAllEvents,
-    getEventById
-}
+export const addInterestedEvent = async (eventId, authId) => {
+  try {
+    await updateInterest(eventId, authId);
+  } catch (error) {
+    throw error;
+  }
+  revalidatePath("/");
+};
+
+export { getAllEvents, getEventById };
